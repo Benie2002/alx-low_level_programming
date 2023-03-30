161221d52594d560918057a2e297d0158fec630b
Benie2002 <isharabenie@gmail.com>
2023-03-30 10:53:54 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include "main.h"
 /**
 * leet - encodes a string into 1337
-* @n: input
-* Return: the value of n
+* @s: input
+* Return: encoded string
 */
-char *leet(char *n)
+char *leet(char *s)
 {
-int i, j;
-char s1[] = "aAeEo0tTlL";
-char s2[] = "4433007711";
-for (i = 0; n[i] != '\0'; i++)
+char *leet_str;
+char leet_table[5] = {'4', '3', '0', '7', '1'};
+char original_chars[5] = {'a', 'e', 'o', 't', 'l'};
+char original_chars_upper[5] = {'A', 'E', 'O', 'T', 'L'};
+int i;
+int j;
+leet_str = s;
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j < 5; j++)
 {
-if (n[i] == s1[j])
+if (s[i] == original_chars[j] || s[i] == original_chars_upper[j])
 {
-n[i] = s2[j];
+leet_str[i] = leet_table[j];
+break;
 }
 }
 }
-return (n);
+return (leet_str);
 }
